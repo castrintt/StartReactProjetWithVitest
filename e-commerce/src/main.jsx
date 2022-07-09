e6d74269d63d2f8c2e6835a2697890b03b4bc775
castro <igor.castro@xbits.com.br>
2022-07-09 17:16:42 -0300

    ICA the base of the project is already done

--- e-commerce/src/main.jsx
+++ e-commerce/src/main.jsx
@@ -1,12 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<App />} />
-    </Routes>
-    </BrowserRouter>
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
